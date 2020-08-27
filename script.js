3467be4e3fde0ce96a670d22478e884436df2e97
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-27 16:29:38 +0200

    extract styles and scripts from html to seseparate files

---
+++ script.js
@@ -0,0 +1,70 @@
+ // D3 imports
+        const { 
+            select, 
+            json,
+            scaleLinear,
+            scaleBand,
+            max,
+            axisLeft,
+            axisBottom
+        } = d3;
+
+        // Source data
+        const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+
+        // Svg dimensions
+        const width = 500;
+        const height = 500;
+
+        // Svg
+        const svg = select('svg')
+            .attr('width', width)
+            .attr('height', height);
+
+        // Render function
+        const render = data => {
+
+            // Value accessors
+            const xValue = d => d[1];
+            const yValue = d => d[0];
+
+            // Margins
+            const margin = { top: 20, right: 20, bottom: 20, left: 100 };
+            const innerWidth = width - margin.left - margin.right;
+            const innerHeight = height - margin.top - margin.bottom;
+
+            // x scale
+            const xScale = scaleLinear()
+                .domain([0, max(data, xValue)])
+                .range([0, innerWidth]);
+
+            // y scale
+            const yScale = scaleBand()
+                .domain(data.map(yValue))
+                .range([0, innerHeight])
+                .padding(.2);
+
+            // Group element
+            const g = svg.append('g')
+                .attr('transform', `translate(${margin.left},${margin.top})`);
+
+            // Axes
+            g.append('g').call(axisLeft(yScale));
+            g.append('g').call(axisBottom(xScale))
+                .attr('transform', `translate(0,${innerHeight})`);
+
+            // Render rectangles (bars)
+            g.selectAll('rect').data(data)
+                .enter().append('rect')
+                    .attr('width', d => xScale(xValue(d)))
+                    .attr('height', d => yScale.bandwidth())
+                    .attr('y', d => yScale(yValue(d)));
+        }
+
+        // Make http request using json method from d3
+        json(url).then(sourceData => {
+            const { data } = sourceData;
+
+            // Render only part of the data during development
+            render(data.slice(0, 20));
+        });
