9b56b3841b32eb6e5b65c82ab6270c33977fb63f
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-27 16:36:14 +0200

    add root container

--- script.js
+++ script.js
@@ -1,70 +1,85 @@
- // D3 imports
-        const { 
-            select, 
-            json,
-            scaleLinear,
-            scaleBand,
-            max,
-            axisLeft,
-            axisBottom
-        } = d3;
+// D3 imports
+const {
+  select,
+  json,
+  scaleLinear,
+  scaleBand,
+  min,
+  max,
+  axisLeft,
+  axisBottom,
+} = d3;
 
-        // Source data
-        const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+// Source data
+const url =
+  'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 
-        // Svg dimensions
-        const width = 500;
-        const height = 500;
+const rootPadding = 50;
 
-        // Svg
-        const svg = select('svg')
-            .attr('width', width)
-            .attr('height', height);
+// Svg dimensions
+const width = 500;
+const height = window.innerHeight - 50;
 
-        // Render function
-        const render = data => {
+// Root
+const root = select('#root');
 
-            // Value accessors
-            const xValue = d => d[1];
-            const yValue = d => d[0];
+// Svg
+const svg = root.append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-            // Margins
-            const margin = { top: 20, right: 20, bottom: 20, left: 100 };
-            const innerWidth = width - margin.left - margin.right;
-            const innerHeight = height - margin.top - margin.bottom;
+// Render function
+const render = (data) => {
+  // Value accessors
+  const xValue = (d) => d[1];
+  const yValue = (d) => d[0];
 
-            // x scale
-            const xScale = scaleLinear()
-                .domain([0, max(data, xValue)])
-                .range([0, innerWidth]);
+  // Margins
+  const margin = { top: 20, right: 20, bottom: 20, left: 100 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
-            // y scale
-            const yScale = scaleBand()
-                .domain(data.map(yValue))
-                .range([0, innerHeight])
-                .padding(.2);
+  // x scale
+  const xScale = scaleLinear()
+    .domain([0, max(data, xValue)])
+    .range([0, innerWidth]);
 
-            // Group element
-            const g = svg.append('g')
-                .attr('transform', `translate(${margin.left},${margin.top})`);
+  // y scale
+  const yScale = scaleBand()
+    .domain(data.map(yValue))
+    .range([0, innerHeight])
+    .padding(0.2);
 
-            // Axes
-            g.append('g').call(axisLeft(yScale));
-            g.append('g').call(axisBottom(xScale))
-                .attr('transform', `translate(0,${innerHeight})`);
+  // Group element
+  const g = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
-            // Render rectangles (bars)
-            g.selectAll('rect').data(data)
-                .enter().append('rect')
-                    .attr('width', d => xScale(xValue(d)))
-                    .attr('height', d => yScale.bandwidth())
-                    .attr('y', d => yScale(yValue(d)));
-        }
+  // Title 
+  g.append('text')
+    .attr('id', 'title')
+    .text('Gross Domestic');
 
-        // Make http request using json method from d3
-        json(url).then(sourceData => {
-            const { data } = sourceData;
+  // Axes
+  g.append('g').call(axisLeft(yScale));
+  g.append('g')
+    .call(axisBottom(xScale))
+    .attr('transform', `translate(0,${innerHeight})`);
 
-            // Render only part of the data during development
-            render(data.slice(0, 20));
-        });
+  // Render rectangles (bars)
+  g.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('width', (d) => xScale(xValue(d)))
+    .attr('height', (d) => yScale.bandwidth())
+    .attr('y', (d) => yScale(yValue(d)));
+};
+
+// Make http request using json method from d3
+json(url).then((sourceData) => {
+  const { data } = sourceData;
+
+  // Render only part of the data during development
+  render(data.slice(0, 20));
+});
